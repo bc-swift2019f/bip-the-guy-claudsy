a3e74ab764b719a1c3bc12e669a3fad2f8f27de2
claudsy <claudsy>
2019-09-16 11:06:10 -0400

    4.1 complete

--- Bip The Guy/ViewController.swift
+++ Bip The Guy/ViewController.swift
@@ -10,11 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func upBy200Pressed(_ sender: UIButton) {
+        imageView.frame.origin.y -= 200
+    }
+    
+    
+    
+    @IBAction func expandBy60Pressed(_ sender: Any) {
+    }
+    
+    @IBAction func fadeOutPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func pushRightPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func rotate180Pressed(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func slideRight100Pressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func changeBackgroundPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func resetPressed(_ sender: UIButton) {
+    }
+    
+    
 }
 
