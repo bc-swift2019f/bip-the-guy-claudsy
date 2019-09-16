062dec38c2089011d04238ecee976ba71e354154
claudsy <claudsy>
2019-09-16 11:30:41 -0400

    4.1 (practice?) complete

--- Bip The Guy/ViewController.swift
+++ Bip The Guy/ViewController.swift
@@ -18,12 +18,17 @@ class ViewController: UIViewController {
     }
 
     @IBAction func upBy200Pressed(_ sender: UIButton) {
-        imageView.frame.origin.y -= 200
+        UIView.animate(withDuration: 0.5, animations:
+            { self.imageView.frame.origin.y -= 200})
     }
     
     
     
     @IBAction func expandBy60Pressed(_ sender: Any) {
+        let largerRec = CGRect(x: imageView.bounds.origin.x - 60, y: imageView.bounds.origin.y - 60, width: imageView.bounds.width + 60, height: imageView.bounds.height + 60)
+        
+        UIView.animate(withDuration: 0.2, delay: 0.0, usingSpringWithDamping: 0.2, initialSpringVelocity: 10, animations:
+            { self.imageView.bounds = largerRec})
     }
     
     @IBAction func fadeOutPressed(_ sender: UIButton) {
